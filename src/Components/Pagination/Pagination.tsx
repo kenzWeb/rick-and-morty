94ad533b5e5e001a010c6f536c359081f01aba1b
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-05-14 01:15:08 +0300

    fix

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -24,6 +24,7 @@ const Pagination = ({pages}: Props) => {
 				<button
 					key={number}
 					onClick={() => dispatch(setFilter({key: 'page', value: number}))}
+					className={styles.button}
 				>
 					{number}
 				</button>
